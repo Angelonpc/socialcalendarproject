6277feae7e45e232051ea8376b1c602c2f43d228
geovel72 <geovel@sch.gr>
2017-01-02 15:41:30 +0200

    Add files via upload

---
+++ views/login_view.php
@@ -0,0 +1,70 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Calendar: User Login Form</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>">
+	<link rel="icon" type="image/png" href="<?php echo base_url("assets/images/calendar-icon.png"); ?>"/>
+</head>
+<body>
+<nav class="navbar navbar-inverse" role="navigation">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="<?php echo base_url(); ?>index.php/home"><span class="glyphicon glyphicon-home"></span> Calendar</a>
+		</div>
+		<div class="collapse navbar-collapse" id="navbar1">
+			<ul class="nav navbar-nav navbar-right">
+				<?php if ($this->session->userdata('login')){ ?>
+				<li><p class="navbar-text">Hello <?php echo $this->session->userdata('uname'); ?></p></li>
+				<li><a href="<?php echo base_url(); ?>index.php/home/logout"><span class="glyphicon glyphicon-log-out"></span> Log Out</a></li>
+				<?php } else { ?>
+				<li><a href="<?php echo base_url(); ?>index.php/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+				<li><a href="<?php echo base_url(); ?>index.php/signup"><span class="glyphicon glyphicon-user"></span> Signup</a></li>
+				<?php } ?>
+			</ul>
+		</div>
+	</div>
+</nav>
+<br/>
+<div class="container">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4 well">
+		<?php $attributes = array("name" => "loginform");
+			echo form_open("login/index", $attributes);?>
+			<legend>Login</legend>
+			<div class="form-group">
+				<label for="emailid">Email-ID</label>
+				<input class="form-control" name="email" placeholder="Enter Email-ID" type="text" id ="emailid" value="<?php echo set_value('email'); ?>" />
+				<span class="text-danger"><?php echo form_error('email'); ?></span>
+			</div>
+			<div class="form-group">
+				<label for="passwordid">Password</label>
+				<input class="form-control" name="password" placeholder="Password" type="password" id="passwordid" value="<?php echo set_value('password'); ?>" />
+				<span class="text-danger"><?php echo form_error('password'); ?></span>
+			</div>
+			<div class="form-group">
+				<button name="submit" type="submit" class="btn btn-info">Login</button>
+				<button name="cancel" type="reset" class="btn btn-info">Cancel</button>
+			</div>
+		<?php echo form_close(); ?>
+		<?php echo $this->session->flashdata('msg'); ?>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4 text-center">	
+		New User? <a href="<?php echo base_url(); ?>index.php/signup">Sign Up Here</a>
+		</div>
+	</div>
+</div>
+<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.10.2.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+</body>
+</html>
